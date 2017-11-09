084b96f39551d438674f2cee115678f63522ac3b
NEETHU <neethusanthosh01@gmail.com>
2017-11-09 15:28:09 +0100

    added one to one association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :events
+
+  has_one :profile
+
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
+
+  def full_name
+    profile.full_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :events, except: [:destroy]
+  resources :profiles, only: [:new, :edit, :create, :update]
 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
